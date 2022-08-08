9ad49eb7d3d0f3d94883db2024f11107dc97994e
EPKunis <epkunis@student.FullSail.edu>
2022-08-08 10:27:16 -0400

    Spawners work
    Spawners work going to work on it spawing in more if the kill condition is met

--- UnityZombieAttack/Assets/Scripts/spawner.cs
+++ UnityZombieAttack/Assets/Scripts/spawner.cs
@@ -7,6 +7,8 @@ public class spawner : MonoBehaviour
     [SerializeField] int numEnemiesToSpawn;
     [SerializeField] int timer;
     [SerializeField] GameObject enemy;
+    [SerializeField] GameObject Spitter;
+    [SerializeField] GameObject Sprinter;
 
     int spawnedEnemyNum;
     bool canSpawn = true;
@@ -14,16 +16,52 @@ public class spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     IEnumerator spawnEnemy()
     {
+        //int spawnThatEnemy = Random.RandomRange(1, 3);
+
+
+        //if (spawnedEnemyNum == 1)
+        //{
+        //    canSpawn = false;
+        //    Instantiate(enemy, transform.position, enemy.transform.rotation);
+        //    spawnedEnemyNum++;
+        //    yield return new WaitForSeconds(timer);
+        //    canSpawn = true;
+        //}
+
+        //if (spawnedEnemyNum == 2)
+        //{
+        //    canSpawn = false;
+        //    Instantiate(Spitter, transform.position, enemy.transform.rotation);
+        //    spawnedEnemyNum++;
+        //    yield return new WaitForSeconds(timer);
+        //    canSpawn = true;
+        //}
+
+        //if (spawnedEnemyNum == 3)
+        //{
+        //    canSpawn = false;
+        //    Instantiate(Sprinter, transform.position, enemy.transform.rotation);
+        //    spawnedEnemyNum++;
+        //    yield return new WaitForSeconds(timer);
+        //    canSpawn = true;
+        //}
+
+
+
         canSpawn = false;
         Instantiate(enemy, transform.position, enemy.transform.rotation);
         spawnedEnemyNum++;
         yield return new WaitForSeconds(timer);
         canSpawn = true;
+
+
+
+
     }
 
     // Update is called once per frame
