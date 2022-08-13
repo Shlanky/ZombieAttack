9fdad754a4cf0d06003acda8f41dce7b12bb4121
EPKunis <epkunis@student.FullSail.edu>
2022-08-13 10:14:17 -0400

    Music in maze
    Added folder in prefab for music put the music prefabs in there

--- UnityZombieAttack/Assets/Scripts/Music.cs
+++ UnityZombieAttack/Assets/Scripts/Music.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class Music : MonoBehaviour
 {
     public GameObject[] music;
-    //have dif music w dif tags so we can run if statements and play it based on where it is
     int gameModeHolder;
-   [SerializeField] int scene_num;
+    [SerializeField] int scene_num;
 
     private void Awake()
     {
-       music = GameObject.FindGameObjectsWithTag("Music");
+        music = GameObject.FindGameObjectsWithTag("Music");
         if (music.Length > 1)
         {
             Destroy(this.gameObject);
